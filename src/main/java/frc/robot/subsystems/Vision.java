89efeca62b4092d29ed688e16d0fe093b5400c3a
Cool Erik <ErikSLee@gmail.com>
2023-11-11 17:24:32 -0600

    PhotonVision

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,11 +1,17 @@
 package frc.robot.subsystems;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
+    private static final double CAMERA_HEIGHT_METERS = 0.7;
+    private static final double TARGET_HEIGHT_METERS = 0.5;
+    private static final double CAMERA_PITCH_RADIANS = 0.0;
     PhotonCamera camera;
     final double ANGULAR_P = 0.1;
     final double ANGULAR_D = 0.0;
@@ -27,5 +33,22 @@ public class Vision extends SubsystemBase {
             rotationSpeed = 0;
         }
         return rotationSpeed;
+    }
+
+    @Override
+    public void periodic() {
+        var result = camera.getLatestResult();
+        double range =
+        PhotonUtils.calculateDistanceToTargetMeters(
+                CAMERA_HEIGHT_METERS,
+                TARGET_HEIGHT_METERS,
+                CAMERA_PITCH_RADIANS,
+                Units.degreesToRadians(result.getBestTarget().getPitch()));
+                
+        SmartDashboard.putBoolean("Has Targets", result.hasTargets());
+        SmartDashboard.putNumber("Yaw to Target", result.getBestTarget().getYaw());
+        SmartDashboard.putNumber("Range to Target", range);
+        
+
     }
 }
